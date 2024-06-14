e10bb4e77c71e7b379feae685b35c61e12bc66d0
rverma-dev <rverma.dev@gmail.com>
2024-06-14 19:33:37 +0530

    minor refactor create-post

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -16,19 +16,24 @@ export function CreatePost() {
     },
   });
 
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createPost.mutate({ name });
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        createPost.mutate({ name });
-      }}
-      className="flex flex-col gap-2"
-    >
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
       <input
         type="text"
         placeholder="Title"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={handleChange}
         className="w-full rounded-full px-4 py-2 text-black"
       />
       <button
